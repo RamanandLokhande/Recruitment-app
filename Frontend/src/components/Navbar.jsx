ce895634bda3a3442384b881fba53b4e41b951a4
Rohit1865 <rohityamgar18@gmail.com>
2025-06-27 15:07:34 +0530

    added app and nav

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+// src/components/Navbar.jsx
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="p-4 bg-black text-white flex justify-between items-center">
+      <div className="text-3xl font-bold text-blue-600">Skillize</div>
+      <div className="space-x-4">
+        <button
+          onClick={() => navigate('/blog')}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Blog
+        </button>
+
+        <button
+          onClick={() => navigate('/Menu')}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Menu
+
+        </button>
+
+        <button
+          onClick={() => navigate('/login')}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Login
+        </button>
+        <button
+          onClick={() => navigate('/signup')}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Sign Up
+        </button>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
