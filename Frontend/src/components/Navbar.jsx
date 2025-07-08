775ae3796b4730c9a44cb7c8f75a6a1d5fbf7a3f
swaraj20s <swaraj041205@gmail.com>
2025-07-08 15:13:18 +0530

    white and fixed navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -7,9 +7,9 @@ function Navbar() {
   const navigate = useNavigate();
 
   return (
-    <nav className="p-4 bg-black text-white flex justify-between items-center">
-      <div className="text-3xl font-bold text-blue-600">Skillize</div>
-      <div className="space-x-4">
+    <nav className="fixed top-0 left-0 w-full p-4 bg-white text-white flex justify-between items-center">
+      <div className="text-3xl font-bold text-blue-600">Nextintern</div>
+      <div className="space-x-3">
          <button
           onClick={() => navigate('/blog')} 
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
